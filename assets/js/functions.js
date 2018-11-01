80b02854eac2b5e239a7c76f82134233802ddca7
amircisija <amircisija@gmail.com>
2018-11-01 22:25:01 +0100

    #

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,18 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  $(".bgimage").css({ height: $(window).height() + "px" });
 
-  // Get started!
+  $(window).on("resize", function() {
+    $(".bgimage").css({ height: $(window).height() + "px" });
+  });
+});
 
+const body = document.body;
+const btn = document.querySelectorAll(".button")[0];
+
+btn.addEventListener("mouseenter", () => {
+  body.classList.add("show");
+});
+
+btn.addEventListener("mouseleave", () => {
+  body.classList.remove("show");
 });
